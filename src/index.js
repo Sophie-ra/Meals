5d15d17329b1ea83d6a127a13e2eacd4eedaa81e
Sophie-ra <91337941+Sophie-ra@users.noreply.github.com>
2022-06-06 13:50:00 +0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+import {AppContext} from './components/context/context'
+
+ReactDOM.render(
+  <React.StrictMode>
+      <AppContext>
+          <App />
+
+      </AppContext>
+
+  </React.StrictMode>,
+  document.getElementById('root')
+  
+);
+
+
+
+
